0abb8b11b5e8cf62695ed8e5c8726824a42c27ac
Hugo Sales Corrêa <hugosalescorrea@gmail.com>
2014-11-29 09:16:23 -0300

    Update algebratest.cpp

--- source/algebratest.cpp
+++ source/algebratest.cpp
@@ -9,7 +9,25 @@ int main () {
     matrix a {c1,c2,c3};
     m_vector b {7,-8,6};
 
-    gauss_jacobi(a,b,m_vector{0.95,-1.75,0.95}).print();
+    matrix inv = iterative_inverse(a,gauss_jacobi);
+
+    cout << "-------------\n";
+
+    inv.print();
+
+    cout << "-------------\n";
+
+    vector<m_vector> columns;
+    columns.push_back(c1);
+    columns.push_back(c2);
+    columns.push_back(c3);
+
+    matrix a1 (columns);
+
+    a1.print();
+    cout << "hehe\n";
+
+    (a*inv).print();
 
     return 0;
 }
